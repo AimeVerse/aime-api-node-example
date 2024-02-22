3427edfc95cef3e184cd15002cbbf4c1ba712a9d
Captain Crypto <tamir@crypto-knight.tech>
2024-02-22 14:53:39 +0200

    env example

--- src/aime-api-client.ts
+++ src/aime-api-client.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config'
-import { _x, Wormholes, _xem, WormholeEvents, _xlog } from "xpell-node"
+import { _x, _xem, _xlog } from "xpell-node"
 
 import {AimeAPI} from "./aime/api.js"
 import { AimeSession } from './aime/api-types.js'
@@ -15,6 +15,7 @@ async function main() {
     
     //create an instance of the AimeAPI
     const aimeApi = new AimeAPI(<string>wormholeToken,<string>wormholeUrl)
+    
     //connect to the server
     await aimeApi.connect()
     
@@ -33,54 +34,27 @@ async function main() {
 
         //send a prompt to the avatar and display the response
         const prompt = "hi, my name is bond, james bond"
-        _xlog.log("************************");
         
-        _xlog.log("Sending prompt to the Avatar->" + prompt)
+        _xlog.log("Q:" ,prompt)
         const answer1 = await aimeApi.sendPrompt(prompt,session)
-        _xlog.log("Avatar response:\n",answer1._npc_response)
+        _xlog.log("A:",answer1._npc_response)
         _xlog.log("Response conversation-item-id",answer1._conversation_item_id)
 
 
         //send another prompt to the avatar and display the response
         const prompt2 = "what is aime avatar?"
-        _xlog.log("************************");
         _xlog.log("Q:" , prompt2)
         const answer2 = await aimeApi.sendPrompt(prompt2,session)
         _xlog.log("A:",answer2._npc_response)
         _xlog.log("Response conversation-item-id:",answer2._conversation_item_id)
-        
+
     } catch (error) {
         _xlog.error("Error starting anonymous session")
     } finally {
         aimeApi.disconnect()
     }
-
-
-    
-
-
 }
 
-// async function startChat() {
-
-
-//     const prompt = "Tell me about the moon"
-
-//     //get an answer from the aime server
-//     const getAnswerCommand = AimeApi.getAnswerXMessage(userId, conversationId, prompt)
-//     const messageResponse = await Wormholes.sendSync(getAnswerCommand, true)
-//     console.log("Q: " + prompt)
-//     console.log("Aime response -> ",messageResponse)
-
-//     const prompt2 = "What is the capital of Israel?"
-//     const getAnswer2 = AimeApi.getAnswerXMessage(userId, conversationId, prompt2)
-//     const messageResponse2 = await Wormholes.sendSync(getAnswer2, true)
-//     console.log("Q: " + prompt2)
-//     console.log("Aime response -> ")
-//     console.dir(messageResponse2)
-//     Wormholes.close()
-    
-// }
 
 
 main().then(async () => {
